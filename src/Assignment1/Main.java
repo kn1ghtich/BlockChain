e4d0fb6c07db5e6e9733f54a538a5f186dc36fd3
qb2k <153792298+qb2k@users.noreply.github.com>
2024-12-31 14:27:54 +0500

    Daniil

--- src/Assignment1/Main.java
+++ src/Assignment1/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Main {
 
-
+  
     public static void main(String[] args) {
         System.out.println("-=| RSA Demonstration |=-");
         RSA rsa = new RSA();
